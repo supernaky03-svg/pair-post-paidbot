037af325ea632da38cda8e330fbc9b693a8408ab
supernaky03-svg <supernaky03@gmail.com>
2026-04-22 21:54:17 +0630

    Add waiting_remove_url_rule to multiple states

--- app/bot/states.py
+++ app/bot/states.py
@@ -1,8 +1,10 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class OtpStates(StatesGroup):
     waiting_otp = State()
     waiting_restore_choice = State()
+
 
 class AddPairStates(StatesGroup):
     waiting_pair_no = State()
@@ -12,28 +14,35 @@ class AddPairStates(StatesGroup):
     waiting_ads = State()
     waiting_post_rule = State()
     waiting_forward_rule = State()
+    waiting_remove_url_rule = State()
     waiting_confirm = State()
+
 
 class DeletePairStates(StatesGroup):
     waiting_pair_no = State()
     waiting_confirm = State()
 
+
 class EditSourceStates(StatesGroup):
     waiting_pair_no = State()
     waiting_source = State()
     waiting_scan = State()
+    waiting_remove_url_rule = State()
     waiting_confirm = State()
+
 
 class EditTargetStates(StatesGroup):
     waiting_pair_no = State()
     waiting_target = State()
     waiting_confirm = State()
 
+
 class KeywordStates(StatesGroup):
     waiting_pair = State()
     waiting_action = State()
     waiting_add_values = State()
     waiting_clear_values = State()
+
 
 class AdsStates(StatesGroup):
     waiting_action = State()
@@ -42,10 +51,12 @@ class AdsStates(StatesGroup):
     waiting_values = State()
     waiting_delete_confirm = State()
 
+
 class RuleStates(StatesGroup):
     waiting_pair = State()
     waiting_value = State()
 
+
 class CheckStates(StatesGroup):
     waiting_pair = State()
-
+    
